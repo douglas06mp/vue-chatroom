7be12f535116410428acb10da073c3fcc3906595
douglas <douglas06mp@gmail.com>
2020-02-26 15:51:55 +0800

    style Login component

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <template>
-  <div id="app">
+  <div id="app" class="h-100 d-flex justify-content-center align-items-center">
     <router-view />
   </div>
 </template>
 
 <style lang="scss">
 #app {
+  background: linear-gradient(to top, #7f7fd5, #86a8e7, #91eae4);
 }
 </style>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,5 +1,20 @@
 <template>
-  <div>
-    <h1>Login</h1>
+  <div id="login" class="rounded p-3">
+    <div class="w-100 mb-3">
+      <h4 class="text-white d-inline-block">Enter your name</h4>
+      <button class="btn btn-primary rounded-circle float-right">
+        <i class="fas fa-arrow-right"></i>
+      </button>
+    </div>
+    <div class="input-group">
+      <input type="text" class="form-control" placeholder="name" />
+    </div>
   </div>
 </template>
+
+<style lang="scss" scoped>
+#login {
+  background: rgba(0, 0, 0, 0.3);
+  width: 350px;
+}
+</style>
