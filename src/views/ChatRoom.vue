c8d5c5034767fa24525dea72f3418333f40dc381
douglas <douglas06mp@gmail.com>
2020-02-26 16:38:02 +0800

    style ChatRoom component

--- src/views/ChatRoom.vue
+++ src/views/ChatRoom.vue
@@ -1,5 +1,37 @@
 <template>
-  <div>
-    <h1>Chatroom</h1>
+  <div id="chatroom" class="container d-flex justify-content-between">
+    <div class="info h-100 d-flex flex-column justify-content-between">
+      <div class="block">Name</div>
+      <div class="block">Room</div>
+    </div>
+    <div class="block room">
+      <h1>Chat</h1>
+    </div>
   </div>
 </template>
+
+<style lang="scss" scoped>
+#chatroom {
+  height: 800px;
+  .info {
+    flex-basis: 35%;
+    .block {
+      &:first-child {
+        flex-basis: 20%;
+      }
+      &:last-child {
+        flex-basis: 75%;
+      }
+    }
+  }
+
+  .room {
+    flex-basis: 60%;
+  }
+
+  .block {
+    background: rgba(0, 0, 0, 0.3);
+    border-radius: 15px;
+  }
+}
+</style>
